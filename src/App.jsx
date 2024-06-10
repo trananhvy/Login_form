8a2db6e9df7947dc29746a33ccfd97c20d124aa7
TranAnhVy <trananhvy9420@gmail.com>
2024-06-10 22:21:56 +0700

    bruh

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import Login from "./components/Login";
+function App() {
+  return (
+    <>
+      <Login />
+    </>
+  );
+}
+
+export default App;
